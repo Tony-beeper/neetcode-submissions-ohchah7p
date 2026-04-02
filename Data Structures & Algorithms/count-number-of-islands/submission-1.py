6d912a6d5244afae5fb4124913c218b340245a54
Tony Tan <tyx1999120755@gmail.com>
2026-04-02 12:03:14 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+
+        visited = [[False for _ in range(m)] for _ in range(n)]
+        # print(visited)
+        counter = 0
+        WATER = "0"
+        LAND = "1"
+        dirs = [(0,1),(1,0), (-1,0), (0,-1)]
+        for i in range(n):
+            for j in range(m):
+                # print(grid[i][j])
+                if grid[i][j] == LAND and not visited[i][j]:
+                    # print("debug")
+                    counter += 1
+                    q = deque()
+                    q.append((i,j))
+                    while q:
+                        a,b = q.popleft()
+                        visited[a][b] = True
+                        for x, y in dirs:
+                            nr = x+a
+                            nc = b + y
+                            if nr >= 0 and nc >= 0 and nr < n and nc < m:
+                                if grid[nr][nc] == LAND and not visited[nr][nc]:
+                                    q.append((nr,nc))
+        # print(visited)
+        return counter
+        
+
+
+
+
+
+
+
+
+
+
