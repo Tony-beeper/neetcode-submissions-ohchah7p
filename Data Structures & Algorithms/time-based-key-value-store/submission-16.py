1eda80f57af4c4442df3ba7f7e90dc3902e65bf1
Tony Tan <tyx1999120755@gmail.com>
2026-04-15 23:49:54 -0400

    Update: time-based-key-value-store - submission-16

--- Data Structures & Algorithms/time-based-key-value-store/submission-16.py
+++ Data Structures & Algorithms/time-based-key-value-store/submission-16.py
@@ -2,33 +2,19 @@ import bisect
 class TimeMap:
 
     def __init__(self):
-        self.timemap = defaultdict(list)
-        
+        self.dict = defaultdict(list) 
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.timemap[key].append((timestamp, value))
-
+        self.dict[key].append((value, timestamp))
     def get(self, key: str, timestamp: int) -> str:
-
-        cur_map = self.timemap[key]
-        time = []
-        for i in range(len(cur_map)):
-            time.append(cur_map[i][0])
-        left = 0
-        right = len(cur_map)
-
-        while left < right:
-            mid = (left + right) // 2
-            if cur_map[mid][0] <= timestamp:
-                left = mid + 1
-            else:
-                right = mid
+        if not self.dict.get(key):
+            return ""
         
-        left -= 1
-        left = bisect.bisect_right(time, timestamp) - 1
-        # print(cur_map)
-        # print(left)
-        if 0 <= left < len(cur_map) and cur_map[left][0] <= timestamp:
-            return cur_map[left][1]
+        lst = self.dict.get(key)
+        times = [time for _, time in lst]
+
+        idx = bisect.bisect_right(times, timestamp)
+        if times[idx-1] <= timestamp:
+            return lst[idx-1][0]
         return ""
         
